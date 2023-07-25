6d08dfa263fd4907a20c2677c0f833047dfd00d7
bluesky0115 <gold.star.bee@gmail.com>
2023-07-25 05:21:15 -0700

    change the array

--- src/controller.js
+++ src/controller.js
@@ -40,7 +40,10 @@ const checkCode = async (req, res) => {
     try {
         const _code = req.query.code;
         console.log("_code=", _code);
-        let data = readCodesFromFile();
+        // let data = readCodesFromFile();
+        let data = [
+            {"isUsed":0,"code":"g68hA-dFpe6-Vbz1j-6UVjO"},{"isUsed":0,"code":"plMIl-GXtnV-VObhn-1VXfh"},{"isUsed":0,"code":"83lZ9-MOWXm-jYuiT-IcutT"},{"isUsed":0,"code":"rxspP-7yHcu-cpeRs-sPiAt"},{"isUsed":0,"code":"bwKsJ-XcJX1-NZCU7-autF0"},{"isUsed":0,"code":"bsSj1-v39Rl-jqIU3-ZsFXS"},{"isUsed":0,"code":"5v7uE-exiD3-ccOWe-SiQ6m"},{"isUsed":0,"code":"qlNJF-BboVr-0Oi5P-NKeXi"},{"isUsed":0,"code":"SKQEc-BBe7H-1UOaz-Ddjq1"},{"isUsed":0,"code":"TOUU1-M4HxJ-dKz3F-wEyQk"},{"isUsed":0,"code":"fz62i-kQ6mm-a0Edm-4xn59"},{"isUsed":0,"code":"C69XV-3VoO0-5k5GV-jeUdQ"},{"isUsed":0,"code":"KLw9S-Asipg-INdV1-oW4bY"},{"isUsed":0,"code":"C87I8-ZaCTX-AReAp-YDyYt"},{"isUsed":0,"code":"5hwZq-Suenr-rnnR6-OWUMN"},{"isUsed":0,"code":"NLzHM-h9vli-GyN4V-YrLkP"},{"isUsed":0,"code":"HcCUI-Jo23o-nMRK4-CtP3z"},{"isUsed":0,"code":"RocPb-enoeW-JDRZ4-YRsq6"},{"isUsed":0,"code":"g2tva-qq3ng-KFFRW-TSpLF"},{"isUsed":0,"code":"9xbqD-KTn7A-b0r73-BSEnq"},{"isUsed":0,"code":"CARtC-dJXi9-CcjtU-mQbqP"},{"isUsed":0,"code":"43OnF-9ImTa-XMn5l-DPiks"},{"isUsed":0,"code":"V7NOt-BsnoA-a8smF-drdUt"},{"isUsed":0,"code":"7i2Sw-Z6tw1-AwRK0-r6eAX"},{"isUsed":0,"code":"pdRFD-fPJwB-punbn-Bkulp"},{"isUsed":0,"code":"VkcNY-akSVC-3Dyvc-owDyA"},{"isUsed":0,"code":"uwkvw-cHzzW-zvnvj-LjvyS"},{"isUsed":0,"code":"xu1Q9-F7hPt-2vbPH-cvUgy"},{"isUsed":0,"code":"VP36M-KfWNH-B36K7-wCtlU"},{"isUsed":0,"code":"tLAcr-Jou7G-Vcc2S-qpnID"},{"isUsed":0,"code":"zXipP-5YJj9-NWjAg-rkBZ8"},{"isUsed":0,"code":"CwD5x-UDR2Y-VJ71A-FktPf"}
+        ];
         const _index = data.findIndex(item => item.code == _code);
         if(_index >= 0) {
             if(data[_index].isUsed == 1)  {
@@ -48,7 +51,7 @@ const checkCode = async (req, res) => {
                 return;
             }
             data[_index] = {isUsed: 1, code: _code};
-            writeCodesFromFile(data);
+            // writeCodesFromFile(data);
             res.json({ success: true, data: "You'll get a WL role soon." });
         } else {
             res.json({success: false, data: "Your code is invalid."})
