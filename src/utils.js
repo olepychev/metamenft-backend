6927bf0d6632e99a973a180c844fd82c4da600ee
bluesky0115 <gold.star.bee@gmail.com>
2023-07-26 04:56:16 -0700

    update the errror message

--- src/utils.js
+++ src/utils.js
@@ -86,7 +86,7 @@ const assignRoleToUser = async (username) => {
         console.error("Error assigning role ", err);
         return {
             success: false,
-            message: err
+            message: "Exception error occured. Please try again later."
         }
     }
 }
