d21530fd3197f98dac45feadcf160099a7634397
bluesky0115 <gold.star.bee@gmail.com>
2023-07-26 06:10:20 -0700

    add header for cors

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 app.use("/api", api);
 
 const server = http.createServer(app);
